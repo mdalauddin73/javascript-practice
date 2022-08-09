9017d7ad16ca459e13cdb22fa30479c3627f25e0
Mohammad Alauddin <alaumoon@gmail.com>
2022-08-10 00:20:46 +0600

    add090822-1

---
+++ practice-problems/triangle-area.js
@@ -0,0 +1,14 @@
+//JavaScript Program To Calculate The Area of a Triangle
+function getTriangleArea(a, b, c) {
+    if (a < 0 || b < 0 || c < 0 || (a + b <= c) || a + c <= b || b + c <= a) {
+        console.log("Not a valid triangle");
+        return;
+    }
+    let s = (a + b + c) / 2;
+    const triangleArea = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    return triangleArea.toFixed(2);
+}
+
+const areaOfTriangle = getTriangleArea(4, 5, 7);
+console.log("Area: ", areaOfTriangle);
+
